1a998a2b4c3329dae49681a8ad4efa2ebccf96c6
lucas__almeida <lucaslap27@gmail.com>
2022-08-17 12:26:43 -0400

    Adiciona imagens, faz um scroll lateral entre as imagens e faz navegação para pagina de pedido

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,103 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableHighlight,
+} from "react-native";
 import { Feather, FontAwesome } from "@expo/vector-icons";
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-      <Feather name="shopping-bag" size={24} color="black" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.card}>
+        <ScrollView style={styles.scrollImageCard} horizontal={true}>
+          <Image
+            style={styles.tinyLogo}
+            source={require("../../../assets/pao.jpeg")}
+          />
+          <Image
+            style={styles.tinyLogo}
+            source={require("../../../assets/paoCru.jpeg")}
+          />
+        </ScrollView>
+        <Text style={styles.text}>
+          Delicioso pão caseiro feito com massa artesanal
+        </Text>
+        <Text>R$ 10,00</Text>
+        <View>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => navigation.navigate("Faça seu pedido")}
+          >
+            <Text>Fazer Pedido</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <ScrollView style={styles.scrollImageCard} horizontal={true}>
+          <Image
+            style={styles.tinyLogo}
+            source={require("../../../assets/rosca.jpeg")}
+          />
+          <Image
+            style={styles.tinyLogo}
+            source={require("../../../assets/roscaCrua.jpeg")}
+          />
+        </ScrollView>
+        <Text style={styles.text}>
+          Delicioso rosca caseiro feito com massa artesanal
+        </Text>
+        <Text>R$ 12,00</Text>
+        <View>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => navigation.navigate("Faça seu pedido")}
+          >
+            <Text>Fazer Pedido</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    // marginHorizontal: 20,
+  },
+  card: {
+    marginTop: 15,
+    backgroundColor: "#ccc",
+    padding: 10,
+  },
+  text: {
+    fontSize: 20,
+  },
+  scrollImageCard: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  tinyLogo: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 320,
+    height: 300,
+    marginLeft: 25,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+  button: {
+    marginTop: 5,
+    width: 100,
+    backgroundColor: "#fff",
+    padding: 8,
+    borderRadius: 8,
+  },
+});
